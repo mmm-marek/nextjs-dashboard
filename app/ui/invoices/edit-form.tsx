95f5b544c6041adcd3bd24f4158fa5a365c9962b
Marek Matušica <matusica.marek1@gmail.com>
2024-01-01 19:09:43 +0100

    Add form validation

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 import Link from "next/link";
 import { Button } from "../button";
 import { updateInvoice } from "@/app/lib/actions";
+import { useFormState } from "react-dom";
 
 export default function EditInvoiceForm({
     invoice,
@@ -18,10 +19,12 @@ export default function EditInvoiceForm({
     invoice: InvoiceForm;
     customers: CustomerField[];
 }) {
+    const initialState = { message: null, errors: {} };
     const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+    const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
 
     return (
-        <form action={updateInvoiceWithId}>
+        <form action={dispatch}>
             <div className="rounded-md bg-gray-50 p-4 md:p-6">
                 {/* Invoice ID */}
                 <input type="hidden" name="id" value={invoice.id} />
@@ -117,6 +120,14 @@ export default function EditInvoiceForm({
                         </div>
                     </div>
                 </div>
+            </div>
+            <div>
+                {state.errors?.customerId &&
+                    state.errors.customerId.map((error: string) => (
+                        <p className="mt-2 text-sm text-red-500" key={error}>
+                            {error}
+                        </p>
+                    ))}
             </div>
             <div className="mt-6 flex justify-end gap-4">
                 <Link
